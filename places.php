77f6f6355dcabdbc33d0e38ff8953bcb3537804b
Benjamin Turner <benjamin@passionsplay.com>
2025-02-16 14:43:57 -0800

    feat: add a check for GOOGLE_MAPS_API_KEY env var

--- places.php
+++ places.php
@@ -11,5 +11,19 @@
  * @package         Places
  */
 
+if (!getenv('GOOGLE_MAPS_API_KEY') && file_exists(__DIR__ . '/.env')) {
+	$env = parse_ini_file('.env');
+	putenv("GOOGLE_MAPS_API_KEY=" . $env["GOOGLE_MAPS_API_KEY"]);
+}
+
+if (!getenv('GOOGLE_MAPS_API_KEY')) {
+	error_log('Places plugin requires GOOGLE_MAPS_API_KEY environment variable in order to set the location of a place.');
+} else {
+	add_filter('acf/fields/google_map/api', function ($api) {
+		$api['key'] = getenv('GOOGLE_MAPS_API_KEY');
+		return $api;
+	});
+}
+
 require_once(__DIR__ . '/post-types/place.php');
 ?>
