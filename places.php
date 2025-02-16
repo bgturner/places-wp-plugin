1721245f703f6412ba90dba71173e3f0ef129969
Benjamin Turner <benjamin@passionsplay.com>
2025-02-16 13:37:13 -0800

    initial commit

---
+++ places.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Places
+ * Description:     Organizes media and locations for 'places' CPT.
+ * Author:          Benjamin Turner
+ * Author URI:      https://passionsplay.com
+ * Text Domain:     places
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Places
+ */
+
+// Your code starts here.
