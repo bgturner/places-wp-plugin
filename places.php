4a97810878efd560fd5bffd503d9515a29d62382
Benjamin Turner <benjamin@passionsplay.com>
2025-02-16 16:36:22 -0800

    wip: register 'places' block
    Added boilerplate block code from:
    
    ```
    npx @wordpress/create-block@latest --no-plugin
    ```

--- places.php
+++ places.php
@@ -18,4 +18,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once(__DIR__ . '/verify-google-maps-api.php');
 require_once(__DIR__ . '/post-types/place.php');
 require_once(__DIR__ . '/rest-endpoints/places.php');
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function pp_places_register_places_block_init() {
+	register_block_type( __DIR__ . '/build/places-block' );
+}
+add_action( 'init', 'pp_places_register_places_block_init' );
+
 ?>
