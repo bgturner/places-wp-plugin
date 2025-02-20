feba830067754dc9ea07ef01b3a58565485831bf
Benjamin Turner <benjamin@passionsplay.com>
2025-02-19 20:38:00 -0800

    feat: add acf field to SinglePlace
    Initially, I forgot to set the `location` field in ACF to "Show in
    REST API"

--- src/places-block/SinglePlace.jsx
+++ src/places-block/SinglePlace.jsx
@@ -6,6 +6,7 @@ export const SinglePlace = ({ place }) => {
 		<div className="single-place">
 			<p className="place-title">{place.title.rendered}</p>
 			<p className="place-excerpt">{place.excerpt.raw}</p>
+			<p className="place-address">{place.acf.location.address}</p>
 		</div>
 	);
 };
