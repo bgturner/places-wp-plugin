49dae5c14947344f9ec528280d77e98070f09d13
Benjamin Turner <benjamin@passionsplay.com>
2025-02-19 17:50:30 -0800

    feat: update render.php to render a place from block attributes

--- src/places-block/render.php
+++ src/places-block/render.php
@@ -2,7 +2,21 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
-?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Places – hello from a dynamic block!', 'places' ); ?>
-</p>
+if (! empty($attributes['place']) && is_numeric($attributes['place'])) {
+	$place_id = $attributes['place'];
+	$place = get_post($place_id);
+	if (function_exists('get_field')) {
+		$place_field = get_field('location', $place_id);
+	}
+	?>
+	<div <?php echo get_block_wrapper_attributes(); ?>>
+		<?php if (! empty($place) ) : ?>
+			<h2><?php esc_html_e($place->post_title); ?></h2>
+			<p><?php esc_html_e($place->post_excerpt); ?></p>
+			<?php if (! empty($place_field)) : ?>
+				<p><?php esc_html_e($place_field['address']); ?></p>
+			<?php endif; ?>
+		<?php endif; ?>
+	</div>
+	<?php
+}
